ce6c788f52d63f0ebc968c1d9f06aa3f38f6c78c
PumpkinXD <54535387+PumpkinXD@users.noreply.github.com>
2026-04-14 00:36:22 +0800

    Migrating mapping to mojmap

--- src/main/java/link/infra/mixintrace/mixin/MixinCrashReport.java
+++ src/main/java/link/infra/mixintrace/mixin/MixinCrashReport.java
@@ -1,7 +1,7 @@
 package link.infra.mixintrace.mixin;
 
 import link.infra.mixintrace.TraceUtils;
-import net.minecraft.util.crash.CrashReport;
+import net.minecraft.CrashReport;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,9 +10,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = CrashReport.class)
 public abstract class MixinCrashReport {
-	@Shadow private StackTraceElement[] stackTrace;
+	@Shadow private StackTraceElement[] uncategorizedStackTrace;
 
-	@Inject(method = "addStackTrace", at = @At(value = "FIELD", target = "Lnet/minecraft/util/crash/CrashReport;otherSections:Ljava/util/List;"))
+	@Inject(method = "getDetails(Ljava/lang/StringBuilder;)V", at = @At(value = "FIELD", target = "Lnet/minecraft/CrashReport;details:Ljava/util/List;"))
 	private void mixintrace_addTrace(StringBuilder crashReportBuilder, CallbackInfo ci) {
 		int trailingNewlineCount = 0;
 		// Remove trailing \n
@@ -24,7 +24,7 @@ public abstract class MixinCrashReport {
 			crashReportBuilder.deleteCharAt(crashReportBuilder.length() - 1);
 			trailingNewlineCount++;
 		}
-		TraceUtils.printTrace(stackTrace, crashReportBuilder);
+		TraceUtils.printTrace(uncategorizedStackTrace, crashReportBuilder);
 		crashReportBuilder.append("\n".repeat(trailingNewlineCount));
 	}
 }
--- src/main/java/link/infra/mixintrace/mixin/MixinCrashReportCategory.java
+++ src/main/java/link/infra/mixintrace/mixin/MixinCrashReportCategory.java
@@ -1,18 +1,18 @@
 package link.infra.mixintrace.mixin;
 
 import link.infra.mixintrace.TraceUtils;
-import net.minecraft.util.crash.CrashReportSection;
+import net.minecraft.CrashReportCategory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = CrashReportSection.class)
-public abstract class MixinCrashReportSection {
+@Mixin(value = CrashReportCategory.class)
+public abstract class MixinCrashReportCategory {
 	@Shadow private StackTraceElement[] stackTrace;
 
-	@Inject(method = "addStackTrace", at = @At("TAIL"))
+	@Inject(method = "getDetails", at = @At("TAIL"))
 	private void mixintrace_addTrace(StringBuilder crashReportBuilder, CallbackInfo ci) {
 		TraceUtils.printTrace(stackTrace, crashReportBuilder);
 	}
